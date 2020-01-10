4bf72a69a4bab700dff4b8f7c6c1aecf80bf5543
michaelbeebe123 <beebenmichael@gmail.com>
2020-01-09 21:05:04 -0500

    fixed 'calendar'

--- app/server.js
+++ app/server.js
@@ -6,5 +6,22 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({}))
+// MIDDLEWARE
+app.use(express.urlencoded({ extended:true }));
+app.use(express.json());
 
+// SERVING STATIC ASSETS
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+// CONNECTING TO THE MONGO DB
+mongoose.connect(process.env.MONGODB_URI) || "PATH TO DB"
+
+app.listen(PORT, function() {
+    app.listen(PORT, function() {
+        console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+    })
+});
