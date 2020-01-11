db066aa60b5a00bc038f47034e6652dbef8bee31
michaelbeebe123 <beebenmichael@gmail.com>
2020-01-11 12:15:22 -0500

    got rid of unnecessary component imports

--- app/client/src/components/Nav/index.js
+++ app/client/src/components/Nav/index.js
@@ -3,7 +3,10 @@ import React from 'react';
 // TODO: ADD ROUTES
 function Nav() {
     return (
-        <ul className="nav justify-content-end">
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <a className="nav-link" href="#">Home</a>
+            </li>
             <li className="nav-item">
                 <a className="nav-link" href="#">Medical History</a>
             </li>
