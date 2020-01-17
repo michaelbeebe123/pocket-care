dd3d5790f252e7142f26092c1c9b39762148329e
Stephen Kainec <stephen.kaienc@gmail.com>
2020-01-16 19:06:30 -0500

    fixed the margins on the calendar

--- client/src/components/Calendar/index.js
+++ client/src/components/Calendar/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import "../../App.css";
+import "./style.css";
 // import React,{Component} from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
@@ -12,9 +13,14 @@ import listPlugin from '@fullcalendar/list'
 export default class CalendarComponent extends React.Component {
 
   render() {
-    return (
+   
+      return (
+        <div class="CalendarComponent">
       <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin, timeGridPlugin, listPlugin ]} />
+      </div>
     )
+   
+    
   }
 
 }
